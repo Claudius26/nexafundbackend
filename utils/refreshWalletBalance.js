be7ac782cd5520deb5db40974aae8e7e4f163c55
Claudius26 <osomzed@gmail.com>
2025-12-07 03:39:57 +0100

    added feature

--- utils/refreshWalletBalance.js
+++ utils/refreshWalletBalance.js
@@ -1,10 +1,28 @@
 const { getCryptoPrices } = require("../services/cryptoPrice");
 
+let cachedPrices = null;
+let lastFetched = 0;
+const CACHE_DURATION = 10 * 60 * 1000;
+
+async function getCryptoPricesSafe() {
+  const now = Date.now();
+  if (cachedPrices && now - lastFetched < CACHE_DURATION) return cachedPrices;
+
+  try {
+    cachedPrices = await getCryptoPrices();
+    lastFetched = now;
+    return cachedPrices;
+  } catch (err) {
+    console.error("PRICE FETCH ERROR:", err);
+    return cachedPrices || {};
+  }
+}
+
 async function refreshWalletValues(user) {
-  const coinData = await getCryptoPrices();
+  const coinData = await getCryptoPricesSafe();
 
   user.wallets.forEach(w => {
-    const price = coinData[w.coin]?.price || 0;
+    const price = coinData[w.coin]?.price || 0; 
     w.amountUsd = w.amount * price;
   });
 
